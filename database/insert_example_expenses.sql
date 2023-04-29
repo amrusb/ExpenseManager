f40534fe433dc49009fcafcc0ace8ded1d765801
Bartosz Surma <bartosz.surma@outlook.com>
2023-04-29 14:01:11 +0200

    Zaktualizowanie skryptów SQL

--- database/insert_example_expenses.sql
+++ database/insert_example_expenses.sql
@@ -1,25 +1,30 @@
 USE expense_manager_database;
 
-INSERT INTO expenses(name, category_id, amount, user_id) VALUES('1zakup1', 3, 14.50, 1);
-INSERT INTO expenses(name, category_id, amount, user_id) VALUES('1zakup2', 2, 24.50, 1);
-INSERT INTO expenses(name, category_id, amount, user_id) VALUES('1zakup3', 1, 13.50, 1);
-INSERT INTO expenses(name, category_id, amount, user_id) VALUES('2zakup1', 5, 122.30, 2);
-INSERT INTO expenses(name, category_id, amount, user_id) VALUES('2zakup2', 4, 19.20, 2);
-INSERT INTO expenses(name, category_id, amount, user_id) VALUES('2zakup3', 2, 142.53, 2);
-INSERT INTO expenses(name, category_id, amount, user_id) VALUES('2zakup4', 3, 990.54, 2);
-INSERT INTO expenses(name, category_id, amount, user_id) VALUES('2zakup5', 6, 23.40, 2);
-INSERT INTO expenses(name, category_id, amount, user_id) VALUES('3zakup1', 3, 27.44, 3);
-INSERT INTO expenses(name, category_id, amount, user_id) VALUES('3zakup2', 8, 12.30, 3);
-INSERT INTO expenses(name, category_id, amount, user_id) VALUES('3zakup3', 7, 24.50, 3);
+INSERT INTO expenses(name, category_id,expense_date, amount, user_id) VALUES('Opłata mieszkania', 2, '2023-03-01', 1042.50, 1);
+INSERT INTO expenses(name, category_id,expense_date, amount, user_id) VALUES('Zakup paliwa', 7, '2023-03-04', 250.49, 1);
+INSERT INTO expenses(name, category_id,expense_date, amount, user_id) VALUES('Zakupy', 1, '2023-03-04', 224.36, 1);
+INSERT INTO expenses(name, category_id,expense_date, amount, user_id) VALUES('Wydatek 4', 1, '2023-03-06', 12.30, 1);
+INSERT INTO expenses(name, category_id,expense_date, amount, user_id) VALUES('Bilet na pociag', 2, '2023-03-12', 70.34, 1);
+INSERT INTO expenses(name, category_id,expense_date, amount, user_id) VALUES('Zakupy spożywcze', 1, '2023-03-18', 129.59, 1);
+INSERT INTO expenses(name, category_id,expense_date, amount, user_id) VALUES('Wizyta u lekarza', 4, '2023-03-21', 300.00, 1);
 
-INSERT INTO expenses(name, category_id, amount, user_id) VALUES('4zakup1', 3, 414.50, 4);
-INSERT INTO expenses(name, category_id, amount, user_id) VALUES('4zakup2', 2, 254.50, 4);
-INSERT INTO expenses(name, category_id, amount, user_id) VALUES('4zakup3', 1, 136.50, 4);
--- INSERT INTO expenses(name, category_id, amount, user_id) VALUES('5zakup1', 5, 12.30, 5);
--- INSERT INTO expenses(name, category_id, amount, user_id) VALUES('5zakup2', 4, 9.20, 5);
--- INSERT INTO expenses(name, category_id, amount, user_id) VALUES('5zakup3', 2, 42.53, 5);
--- INSERT INTO expenses(name, category_id, amount, user_id) VALUES('5zakup4', 3, 30.53, 5);
--- INSERT INTO expenses(name, category_id, amount, user_id) VALUES('5zakup5', 6, 231.30, 5);
--- INSERT INTO expenses(name, category_id, amount, user_id) VALUES('6zakup1', 3, 2.44, 6);
--- INSERT INTO expenses(name, category_id, amount, user_id) VALUES('6zakup2', 8, 121.30, 6);
--- INSERT INTO expenses(name, category_id, amount, user_id) VALUES('6zakup3', 7, 22.53, 6);
+INSERT INTO expenses(name, category_id,expense_date, amount, user_id) VALUES('Opłata mieszkania', 2, '2023-04-01', 1024.30, 1);
+INSERT INTO expenses(name, category_id,expense_date, amount, user_id) VALUES('Zakup paliwa', 7, '2023-04-02', 150.49, 1);
+INSERT INTO expenses(name, category_id,expense_date, amount, user_id) VALUES('Zakupy', 1, '2023-04-04', 224.36, 1);
+INSERT INTO expenses(name, category_id,expense_date, amount, user_id) VALUES('Kino', 6, '2023-04-06', 70.99, 1);
+INSERT INTO expenses(name, category_id,expense_date, amount, user_id) VALUES('Basen', 6, '2023-04-12', 30.34, 1);
+INSERT INTO expenses(name, category_id,expense_date, amount, user_id) VALUES('Restauracja', 1, '2023-04-18', 243.59, 1);
+INSERT INTO expenses(name, category_id,expense_date, amount, user_id) VALUES('Zakupy spożywcze', 1, '2023-04-20', 140.20, 1);
+INSERT INTO expenses(name, category_id,expense_date, amount, user_id) VALUES('Benzyna', 7, '2023-04-21', 302.20, 1);
+
+INSERT INTO expenses(name, category_id,expense_date, amount, user_id) VALUES('Opłata mieszkania', 2, '2023-03-02', 942.50, 2);
+INSERT INTO expenses(name, category_id,expense_date, amount, user_id) VALUES('Zakupy', 1, '2023-03-04', 124.36, 2);
+INSERT INTO expenses(name, category_id,expense_date, amount, user_id) VALUES('Telefon', 8, '2023-03-12', 1000.99, 2);
+INSERT INTO expenses(name, category_id,expense_date, amount, user_id) VALUES('Zakupy spożywcze', 1, '2023-03-18', 129.59, 2);
+
+INSERT INTO expenses(name, category_id,expense_date, amount, user_id) VALUES('Mieszkanie', 2, '2023-04-01', 899.78, 2);
+INSERT INTO expenses(name, category_id,expense_date, amount, user_id) VALUES('Rower', 7, '2023-04-02', 2300.99, 2);
+INSERT INTO expenses(name, category_id,expense_date, amount, user_id) VALUES('Opony do roweru', 7, '2023-04-03', 300.26, 2);
+INSERT INTO expenses(name, category_id,expense_date, amount, user_id) VALUES('Teatr', 6, '2023-04-10', 100.99, 2);
+INSERT INTO expenses(name, category_id,expense_date, amount, user_id) VALUES('Zakupy', 1, '2023-04-20', 240.20, 2);
+INSERT INTO expenses(name, category_id,expense_date, amount, user_id) VALUES('Pociąg', 7, '2023-04-21', 54.20, 2);
--- database/insert_example_users.sql
+++ database/insert_example_users.sql
@@ -2,5 +2,3 @@ USE expense_manager_database;
 
 INSERT INTO users(name, password, email) VALUES('nowak', 'haslo', 'nowak@gm.pl');
 INSERT INTO users(name, password, email) VALUES('kowalski', 'haslo', 'kowalski@gm.pl');
-INSERT INTO users(name, password, email) VALUES('wiśniewski', 'haslo', 'wiśniewski@gm.pl');
-INSERT INTO users(name, password, email) VALUES('mazur', 'haslo', 'mazur@gm.pl');
